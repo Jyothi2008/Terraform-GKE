3247c7a99718635fd15b3e0ce74fef5ab640e8d5
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-07-23 01:16:50 +0200

    building the app image

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -14,5 +14,3 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-
-gcloud container clusters get-credentials my-gke --zone us-central1-a --project abdo-project-12345-354211
